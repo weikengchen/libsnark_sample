e5c0e6ed4a8a4d0ea5c18d1a64c373d566d8eb1f
cwk32 <cwk32@mail.ustc.edu.cn>
2018-11-22 12:21:29 -0800

    check whether it is the actual comp

--- sample/src/main-leaf.cpp
+++ sample/src/main-leaf.cpp
@@ -152,6 +152,11 @@ template<typename ppT_A, typename FieldT_A, typename HashT_A> void test_leaf_exa
     libff::bit_vector first_old_root = first_old_hash;
     libff::bit_vector first_new_root = first_new_hash;
     
+    cout << "first_old_root: " << first_old_root << endl;
+    cout << "first_new_root: " << first_new_root << endl;
+    cout << "first_old_leaf: " << first_old_leaf << endl;
+    cout << "first_new_leaf: " << first_new_leaf << endl;
+    
     // declare the constraint system
     LEAF_GADGET
     
@@ -167,7 +172,7 @@ template<typename ppT_A, typename FieldT_A, typename HashT_A> void test_leaf_exa
     address_bits_va.fill_with_bits(pb, address_bits);
     prev_leaf_digest.generate_r1cs_witness(first_old_leaf);
     prev_path_var.generate_r1cs_witness(address, prev_path);    
-    next_leaf_digest.generate_r1cs_witness(first_new_root);
+    next_leaf_digest.generate_r1cs_witness(first_new_leaf);
     
     mls.generate_r1cs_witness();
         
