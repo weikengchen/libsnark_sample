0e72df7a538214f41acdffb159214a6f9b51691e
cwk32 <cwk32@mail.ustc.edu.cn>
2018-11-26 00:07:53 -0800

    understand the encoding

--- sample/src/main-layer2.cpp
+++ sample/src/main-layer2.cpp
@@ -67,12 +67,12 @@ void serialize_bit_vector_nonewline(std::ostream &out, const libff::bit_vector &
 	primary_input_1_bits_second_half.insert(primary_input_1_bits_second_half.end(), primary_input_1_bits.begin() + 300 - 1, primary_input_1_bits.begin() + 593 - 1);\
 	primary_input_1_bits_second_half.insert(primary_input_1_bits_second_half.end(), primary_input_1_bits.begin() + 595 - 1, primary_input_1_bits.begin() + 596 - 1);\
 	bit_vector_copy_gadget<FieldT_B> check_equal_1(pb, prev_root_digest.bits, primary_input_1_bits_first_half, pb_variable<FieldT_B>(1), FieldT_B::capacity(), FMT(annotation, " check_prev_hash_1"));\
+	unpack_input.generate_r1cs_constraints(true);\
 	proof_1.generate_r1cs_constraints();\
 	proof_2.generate_r1cs_constraints();\
 	online_verifier_1.generate_r1cs_constraints();\
 	online_verifier_2.generate_r1cs_constraints();\
 	check_equal_1.generate_r1cs_constraints(false, false);
-	unpack_input.generate_r1cs_constraints(true);\
 
 	//bit_vector_copy_gadget<FieldT_B> check_equal_1(pb, primary_input_1_bits_first_half, prev_root_digest.bits, pb_variable<FieldT_B>(1), FieldT_B::capacity(), FMT(annotation, " check_prev_hash_1"));\
 	//bit_vector_copy_gadget<FieldT_B> check_equal_2(pb, primary_input_1_bits_second_half, primary_input_2_bits_first_half, pb_variable<FieldT_B>(1), FieldT_B::capacity(), FMT(annotation, " check_next_hash_1"));\
@@ -216,7 +216,6 @@ template<typename ppT_A, typename ppT_B> void test_layer2_prove(const std::strin
 	
     //prev_root_digest.generate_r1cs_witness(empty_hash_value);
 	//next_root_digest.generate_r1cs_witness(empty_hash_value);
-	unpack_input.generate_r1cs_constraints(true);
 	proof_1.generate_r1cs_witness(proof_1_in);
 	primary_input_1_bits.fill_with_bits(pb, primary_input_1_as_bits);
 	online_verifier_1.generate_r1cs_witness();
@@ -226,6 +225,7 @@ template<typename ppT_A, typename ppT_B> void test_layer2_prove(const std::strin
 	online_verifier_2.generate_r1cs_witness();
 	pb.val(result_2) = FieldT_B::one();
 	check_equal_1.generate_r1cs_witness();
+	unpack_input.generate_r1cs_constraints(true);
 	//check_equal_2.generate_r1cs_witness();
 	//check_equal_3.generate_r1cs_witness();
         
